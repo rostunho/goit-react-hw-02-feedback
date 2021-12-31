f2cff563e09e4bd7753c4b9dd4bff0cceff6e301
Rostyk Podolets <ros.podolets@gmail.com>
2021-12-31 10:42:21 +0200

    buttons prototype added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
   render() {
-    return <h2>Please leave feedback</h2>;
+    return (
+      <>
+        <h2>Please leave feedback</h2>
+        <ul>
+          <li>
+            <button type="button">Good</button>
+          </li>
+          <li>
+            <button type="button">Neutral</button>
+          </li>
+          <li>
+            <button type="button">Bad</button>
+          </li>
+        </ul>
+      </>
+    );
   }
 }
