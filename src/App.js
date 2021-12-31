cea349df8e32f7939d58e6eda1f817cb2ac045b2
Rostyk Podolets <ros.podolets@gmail.com>
2021-12-31 11:13:49 +0200

    checkpoint markup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
   render() {
     return (
       <>
